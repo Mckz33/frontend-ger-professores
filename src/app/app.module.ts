3d6a79e78cc98d096123962b8f10ba3cde3c62e6
roger-roger-svg <rogercunhadeandrade@gmail.com>
2023-11-18 22:28:36 -0300

    Tela de cadastro : caminho feito , componentes html, consumo de api e etc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { LoginComponent } from './security/login/login.component';
 import { LayoutComponent } from './pages/layout/layout.component';
-
+import { NgSelectModule } from '@ng-select/ng-select';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -24,9 +29,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
-
+    BrowserModule,
+    NgSelectModule,
     FormsModule,
     ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    NgMultiSelectDropDownModule.forRoot(),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/professor/cadastro/cadastro.component.ts
+++ src/app/professor/cadastro/cadastro.component.ts
@@ -1,10 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.css']
+  styleUrls: ['./cadastro.component.css'],
 })
-export class CadastroComponent {
+export class CadastroComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.carregarCursos();
+    this.carregarDisciplinas();
+  }
+  carregarCursos() {
+    this.http.get('http://localhost:3000/cursos').subscribe((data: any) => {
+      this.cursos = data.map((curso: any) => ({
+        value: curso.id,
+        label: curso.nome,
+      }));
+    });
+  }
 
+  carregarDisciplinas() {
+    this.http
+      .get('http://localhost:3000/disciplinas')
+      .subscribe((data: any) => {
+        this.disciplinas = data.map((disciplina: any) => ({
+          value: disciplina.id,
+          label: disciplina.nome,
+        }));
+      });
+  }
+  nome: string = '';
+  tipoContratacao: string = '';
+  horarioDisponivel: number = 0;
+  errorMessage: string = '';
+
+  cursos: { value: string; label: string }[] = [
+    { value: 'ADS', label: 'Análise e Desenvolvimento de Sistemas' },
+    { value: 'RC', label: 'Redes de Computadores' },
+    { value: 'DG', label: 'Design Gráfico' },
+  ];
+
+  // Declaração das disciplinas
+  disciplinas: { value: string; label: string }[] = [
+    { value: 'prog', label: 'Programação' },
+    { value: 'emp', label: 'Empreendedorismo' },
+    { value: 'usa', label: 'Usabilidade' },
+  ];
+  dropdownSettings = {
+    singleSelection: false,
+    idField: 'value',
+    textField: 'label',
+    selectAllText: 'Selecionar Todos',
+    unSelectAllText: 'Desselecionar Todos',
+    allowSearchFilter: true,
+  };
+  curso: string[] = [];
+  disciplina: string[] = [];
+  cadastrarProfessor() {
+    // Verifica se todos os campos foram preenchidos
+    if (
+      typeof this.nome === 'string' &&
+      this.nome.trim() !== '' &&
+      this.cursos.length > 0 &&
+      this.disciplinas.length > 0 &&
+      this.tipoContratacao &&
+      this.horarioDisponivel !== undefined &&
+      !isNaN(Number(this.horarioDisponivel)) &&
+      Number(this.horarioDisponivel) >= 0
+    ) {
+      // Lógica para cadastrar o professor
+      console.log('Professor cadastrado com sucesso!');
+      this.errorMessage = ''; // Limpa a mensagem de erro, caso exista
+
+      alert('Foi cadastrado com sucesso!');
+    } else {
+      this.errorMessage = 'Por favor, preencha todos os campos corretamente.';
+      if (
+        this.horarioDisponivel !== undefined &&
+        Number(this.horarioDisponivel) < 0
+      ) {
+        this.errorMessage += ' O horário disponível não pode ser negativo.';
+      }
+    }
+  }
 }
