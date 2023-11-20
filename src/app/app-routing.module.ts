f06bc5de55c72476211d5df6ca0416adff7fec18
Mckz33 <96853965+Mckz33@users.noreply.github.com>
2023-11-20 12:59:22 -0300

    Revert "Tela de cadastro "

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,25 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './pages/layout/layout.component';
 import { LoginComponent } from './security/login/login.component';
-import { CadastroComponent } from './professor/cadastro/cadastro.component';
 const routes: Routes = [
+
   {
-    path: 'home',
-    component: LayoutComponent,
+    path: 'home', component: LayoutComponent,
   },
 
   {
-    path: 'login',
-    component: LoginComponent,
+    path:'login', component: LoginComponent,
   },
-  { path: 'professor/cadastro', component: CadastroComponent },
 
   // Retorna para a tela Home caso não ache outra página OU url diferente.
-  { path: '**', component: LayoutComponent },
+  {path: '**', component: LayoutComponent}
+
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CadastroComponent } from './professor/cadastro/cadastro.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { LoginComponent } from './security/login/login.component';
 import { LayoutComponent } from './pages/layout/layout.component';
-import { NgSelectModule } from '@ng-select/ng-select';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
-import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CadastroComponent,
     LayoutComponent,
     LoginComponent,
   ],
@@ -29,17 +22,11 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
-    BrowserModule,
-    NgSelectModule,
+
     FormsModule,
     ReactiveFormsModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatInputModule,
-    NgMultiSelectDropDownModule.forRoot(),
-    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
