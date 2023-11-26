0378a2af01d61cf4aaab6534c652dc59a1dc86b2
Mckz33 <mackenzie.max.rj@gmail.com>
2023-11-26 14:02:13 -0300

    implementando jwt do spring security( INCOMPLETO )

--- src/app/services/validacao-cadastro.service.ts
+++ src/app/services/validacao-cadastro.service.ts
@@ -2,11 +2,19 @@
 import { Injectable } from '@angular/core';
 import { Curso } from '../models/curso';
 import { Disciplina } from '../models/disciplina';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const BASE_URL = ['http://localhost:8080/']
+
 @Injectable({
   providedIn: 'root',
 })
 export class ValidacaoCadastroService {
-  constructor() {}
+  
+  constructor(
+    private http: HttpClient
+  ) { }
 
   validarCampos(
     nome: string,
@@ -30,4 +38,34 @@ export class ValidacaoCadastroService {
       return false;
     }
   }
+
+  signup(signupRequest: any): Observable<any> {
+    return this.http.post(BASE_URL + "sign-up", signupRequest)
+  }
+
+  login(loginRequest: any): Observable<any> {
+    return this.http.post(BASE_URL + "authenticate", loginRequest)
+  }
+
+  hello(): Observable<any> {
+    const headers = this.createAuthorizationHeader();
+    
+    return this.http.get(BASE_URL + 'api/hello', {
+      headers: headers || undefined  // Adicionei esta parte para tratar null
+    });
+  }
+  
+
+  private createAuthorizationHeader() {
+    const jwtToken = localStorage.getItem('JWT');
+    if (jwtToken) {
+      return new HttpHeaders().set(
+        'Authorization', 'Bearer ' + jwtToken
+      )
+    } else {
+      console.log("JWT token not found in the Local Storage");
+    }
+    return null;
+  }
+
 }
