549780f5eaedc4a78ed5488059e00ab92f8dfaef
roger-roger-svg <rogercunhadeandrade@gmail.com>
2023-11-29 15:22:36 -0300

    login service

--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -1,45 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder} from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
+import { loginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  formLogin!: FormGroup;
 
-  formLogin!:FormGroup;
-
-  constructor(private fb:FormBuilder, private _http:HttpClient, private router:Router) {
-
-  }
+  constructor(
+    private fb: FormBuilder,
+    private loginservice: loginService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.formLogin = this.fb.group({
-      email:[''],
-      senha:['']
-    })
+      email: [''],
+      senha: [''],
+    });
   }
 
   // Método de Login.
   logIn() {
-    this._http.get<any>("http://localhost:3000/registro").subscribe(res =>{
-      const usuario = res.find((a:any) => {
-        return a.email === this.formLogin.value.email && a.senha === this.formLogin.value.senha
-      })
-      if(usuario) {
-        localStorage.setItem('token', Math.random().toString());
-        alert("Logado com Sucesso!");
-        this.formLogin.reset();
-        this.router.navigate(['home'])
-      } else {
-        alert("Login Inválido.")
-      }
-    }, err => {
-      alert("Login e/ou senha não encontrados.")
-    })
+    const { email, senha } = this.formLogin.value;
+    this.loginservice.authenticateUser(email, senha);
   }
-
 }
