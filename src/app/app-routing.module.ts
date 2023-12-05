dd1e226301284ba6c211c2b43c4ae7cb15d843c8
Mckz33 <mackenzie.max.rj@gmail.com>
2023-12-05 15:03:45 -0300

    integrando com o backend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,27 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
-import { authGuard } from './guard/auth.guard';
+import { authGuard } from './Guard/auth.guard';
 import { HomeComponent } from './pages/home/home.component';
 import { RegistroCoordenadorComponent } from './pages/registro-coordenador/registro-coordenador.component';
 import { ViewCoordenadorComponent } from './pages/view-coordenador/view-coordenador.component';
 import { ProfessorCadastroComponent } from './pages/professor-cadastro/professor-cadastro.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent},
 
-  { path: 'registro', component: RegistroComponent },
+  { path: 'registro', component: RegistroComponent},
 
-  { path: 'registro/coordenador', component: RegistroCoordenadorComponent },
+  { path: 'registro/coordenador', component: RegistroCoordenadorComponent, canActivate: [authGuard]},
 
-  { path: 'registro/professor', component: ProfessorCadastroComponent },
+  { path: 'registro/professor', component: ProfessorCadastroComponent},
 
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent, canActivate: [authGuard]},
 
-  { path: 'coordenador', component: ViewCoordenadorComponent },
+  { path: 'coordenador', component: ViewCoordenadorComponent, canActivate: [authGuard]},
 
   // Retorna para a tela de Login caso não ache outra página OU url diferente.
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
 ];
 
 @NgModule({
